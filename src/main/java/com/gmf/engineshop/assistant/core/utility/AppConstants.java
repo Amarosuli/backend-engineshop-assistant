91e4cea736c6b354ced3209f946b0bb87d24ea40
Amarosuli <vuvlnvw@gmail.com>
2024-02-06 23:47:29 +0700

    [add] AppConstants

---
+++ src/main/java/com/gmf/engineshop/assistant/core/utility/AppConstants.java
@@ -0,0 +1,8 @@
+package com.gmf.engineshop.assistant.core.utility;
+
+public class AppConstants {
+   public static final String CURRENT_PAGE = "0";
+   public static final String TOTAL_ITEMS_PER_PAGE = "5";
+   public static final String SORT_BY = "created";
+   public static final String SORT_ORDER = "asc";
+}
