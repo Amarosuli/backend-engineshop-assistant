419752f082d361f405b5103deb34f54b85724310
Amarosuli <vuvlnvw@gmail.com>
2024-02-07 13:33:05 +0700

    [add] Method findByDeletedIs

--- src/main/java/com/gmf/engineshop/assistant/module/customer/CustomerRepository.java
+++ src/main/java/com/gmf/engineshop/assistant/module/customer/CustomerRepository.java
@@ -1,11 +1,16 @@
 package com.gmf.engineshop.assistant.module.customer;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gmf.engineshop.assistant.module.customer.dto.CustomerDTO;
 
 public interface CustomerRepository<T extends CustomerDTO> extends JpaRepository<T, UUID> {
+   List<CustomerDTO> findByDeletedIs(Boolean isDeleted);
 
+   Page<CustomerDTO> findByDeletedIs(Boolean isDeleted, Pageable pageable);
 }
