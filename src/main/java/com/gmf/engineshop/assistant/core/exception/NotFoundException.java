fd06b647d17b40c89fd6cf8d397750b97db6fa5f
Amarosuli <vuvlnvw@gmail.com>
2024-02-07 23:57:54 +0700

    [add] NotFoundException

---
+++ src/main/java/com/gmf/engineshop/assistant/core/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.gmf.engineshop.assistant.core.exception;
+
+public class NotFoundException extends RuntimeException {
+   public NotFoundException(String message) {
+      super(message);
+   }
+}
