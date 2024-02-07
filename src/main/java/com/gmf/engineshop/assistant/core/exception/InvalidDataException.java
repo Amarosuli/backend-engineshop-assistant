5534dbfce5db2a6371b9e90850d78982e3322e51
Amarosuli <vuvlnvw@gmail.com>
2024-02-07 23:58:08 +0700

    [add] InvalidDataException

---
+++ src/main/java/com/gmf/engineshop/assistant/core/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package com.gmf.engineshop.assistant.core.exception;
+
+public class InvalidDataException extends RuntimeException {
+   public InvalidDataException(String message) {
+      super(message);
+   }
+}
