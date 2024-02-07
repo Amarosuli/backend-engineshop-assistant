f0e26c9a4293b12579e2ea0330818381211ec8f6
Amarosuli <vuvlnvw@gmail.com>
2024-02-07 17:00:51 +0700

    [update] TOTAL_ITEMS_PER_PAGE = 25

--- src/main/java/com/gmf/engineshop/assistant/core/utility/AppConstants.java
+++ src/main/java/com/gmf/engineshop/assistant/core/utility/AppConstants.java
@@ -6,7 +6,7 @@ package com.gmf.engineshop.assistant.core.utility;
  */
 public class AppConstants {
    public static final String CURRENT_PAGE = "0";
-   public static final String TOTAL_ITEMS_PER_PAGE = "5";
+   public static final String TOTAL_ITEMS_PER_PAGE = "25";
    public static final String SORT_BY = "created";
    public static final String SORT_ORDER = "asc";
    public static final String STATUS = "all";
