cf19a29a28903478b039056361081d5bd296a0e6
Amarosuli <vuvlnvw@gmail.com>
2024-02-07 16:15:55 +0700

    [add] ServiceDTO

---
+++ src/main/java/com/gmf/engineshop/assistant/core/base/ServiceDTO.java
@@ -0,0 +1,18 @@
+package com.gmf.engineshop.assistant.core.base;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceDTO<T> {
+   private T data;
+   private String message;
+   private HttpStatus status;
+}
