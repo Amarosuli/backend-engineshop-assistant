6b6b86e96b1fa48fceb7b4d38b7ee4ec952638d7
Amarosuli <vuvlnvw@gmail.com>
2024-02-10 01:18:08 +0700

    [add] EngineModelService

---
+++ src/main/java/com/gmf/engineshop/assistant/module/enginemodel/EngineModelService.java
@@ -0,0 +1,8 @@
+package com.gmf.engineshop.assistant.module.enginemodel;
+
+import com.gmf.engineshop.assistant.core.model.BaseService;
+import com.gmf.engineshop.assistant.module.enginemodel.dto.EngineModelDTO;
+
+public interface EngineModelService extends BaseService<EngineModelDTO> {
+
+}
