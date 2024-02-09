0dddc4b0e11be8fbc24a52ae5d844972cb931aeb
Amarosuli <vuvlnvw@gmail.com>
2024-02-10 01:17:41 +0700

    [add] EngineModelRepository

---
+++ src/main/java/com/gmf/engineshop/assistant/module/enginemodel/EngineModelRepository.java
@@ -0,0 +1,19 @@
+package com.gmf.engineshop.assistant.module.enginemodel;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gmf.engineshop.assistant.module.enginemodel.dto.EngineModelDTO;
+
+public interface EngineModelRepository<T extends EngineModelDTO>
+      extends JpaRepository<T, UUID> {
+   List<T> findByDeletedIs(Boolean isDeleted);
+
+   Page<T> findByDeletedIs(Boolean isDeleted, Pageable pageable);
+
+   T findByName(String name) throws IllegalArgumentException;
+}
