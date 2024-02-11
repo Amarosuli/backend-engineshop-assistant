e30571f704abc9a1d30c6190a2fe3cade8eef5ab
Amarosuli <vuvlnvw@gmail.com>
2024-02-12 05:28:19 +0700

    [add] Create Method with different input type

--- src/main/java/com/gmf/engineshop/assistant/module/enginemodel/EngineModelService.java
+++ src/main/java/com/gmf/engineshop/assistant/module/enginemodel/EngineModelService.java
@@ -1,8 +1,12 @@
 package com.gmf.engineshop.assistant.module.enginemodel;
 
+import java.io.IOException;
+
 import com.gmf.engineshop.assistant.core.model.BaseService;
+import com.gmf.engineshop.assistant.core.model.ServiceDTO;
 import com.gmf.engineshop.assistant.module.enginemodel.dto.EngineModelDTO;
+import com.gmf.engineshop.assistant.module.enginemodel.dto.EngineModelRequestDTO;
 
 public interface EngineModelService extends BaseService<EngineModelDTO> {
-
+   ServiceDTO<EngineModelDTO> create(EngineModelRequestDTO request) throws IOException;
 }
